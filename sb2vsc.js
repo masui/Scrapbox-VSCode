6fa7b0826be404a81040d500c9fdf9c504bde731
Toshiyuki Masui <masui@pitecan.com>
2021-08-16 11:22:17 +0900

    ...

--- sb2vsc.js
+++ sb2vsc.js
@@ -6,4 +6,7 @@ javascript:(function(){
     if(a = location.href.match(/github.dev\/(.*)\/(.*)$/)){
         window.open(`https://Scrapbox.io/${a[2]}`);
     }
+    else if(a = location.href.match(/scrapbox.io\/([\w\-]+)\/?/i)){
+        window.open(`https://github.dev/masui/${a[1]}`);
+    }
 })();
