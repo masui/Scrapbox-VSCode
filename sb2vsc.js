6d32b8ef2c0ad7c7e6010fc23be36376a8c707cb
Toshiyuki Masui <masui@pitecan.com>
2021-08-16 11:37:30 +0900

    新しいウィンドウを開かないように

--- sb2vsc.js
+++ sb2vsc.js
@@ -4,11 +4,11 @@
 javascript:(function(){
     let a;
     if(a = location.href.match(/github.dev\/(.*)\/(.*)$/)){
-        window.open(`https://Scrapbox.io/${a[2]}`);
+        location.href = `https://Scrapbox.io/${a[2]}`;
     }
     else if(a = location.href.match(/scrapbox.io\/([\w\-]+)\/?/i)){
         let title = $('.title')[0].innerHTML;
-        window.open(`https://github.dev/masui/${title}`);
+        location.href = `https://github.dev/masui/${title}`;
     }
 })();
 // タイトル取得手法がイマイチ...
